e33d17bdfa971e8724ad40be949d23c810763e7b
Margarita <mayi@MacBook-Pro-de-Margarita.local>
2020-07-30 17:21:40 +1000

    added services information - Grid

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 import SectionTitleWithText from "../components/section-title/SectionTitleWithText";
 import BannerOne from "../wrappers/banner/BannerOne";
+import TextGridOne from "../wrappers/text-grid/TextGridOne";
 
 const Services = ({ location }) => {
     const { pathname } = location;
@@ -12,8 +13,11 @@ const Services = ({ location }) => {
         {/* Intro/Welcome Services Page - section title with text*/}
         <SectionTitleWithText spaceTopClass="pt-100" spaceBottomClass="pb-95" />
 
-        {/* banner Types of Clients*/}
+        {/* banner Client Types*/}
         <BannerOne spaceBottomClass="pb-70" />
+
+         {/* Services Info - text grid */}
+         <TextGridOne spaceBottomClass="pb-70" />
   
       </Fragment>
     );
